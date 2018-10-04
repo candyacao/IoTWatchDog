a1d094b9380242564a373a498d767b0f7d34b285
CandyaCao <CandyaCao@outlook..com>
2018-10-04 10:12:42 +0800

    数据库中的表结构

---
+++ create.sql
@@ -0,0 +1,352 @@
+create table e_detail_1(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_2(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_3(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_4(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_5(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_6(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_7(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_8(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_9(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_10(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_11(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_12(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_13(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_14(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_15(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_16(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_17(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_18(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_19(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_20(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_21(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_22(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_23(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_24(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_25(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_26(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_27(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_28(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_29(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_30(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+create table e_detail_31(
+name varchar2(20),
+srcId varchar2(5),
+dstId varchar2(5),
+sersorAddress varchar2(7),
+count number(2),
+cmd  varchar2(5),
+status number(2),
+data number(9,4),
+gather_date date
+);
+--------------
+create table u(
+id number(7) constraint u_id_pk primary key,
+username varchar2(10),
+pwd varchar2(10),
+gender varchar2(10),
+info varchar2(50)
+);
+create sequence u_id
+increment by 1
+start with 1;
